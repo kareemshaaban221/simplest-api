34bbde14977fdcc2f016fd085c16254243613a66
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-11-01 00:19:38 +0200

    CROS

--- index.php
+++ index.php
@@ -74,6 +74,7 @@ $method = strtolower($_SERVER['REQUEST_METHOD']);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header('Content-Type: application/json');
 
 switch ($last) {
